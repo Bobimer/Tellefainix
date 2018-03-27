c2da9b1233762fb11055b3c45109d8fa1142b78e
bobimer <remi.beziat@edu.itescia.fr>
2018-03-27 10:55:23 +0200

    database

--- PetitComptable/index.php
+++ PetitComptable/index.php
@@ -6,7 +6,19 @@
 		<title>Banque</title>
   </head>
   <body>
+
     <?php
+    function connect(){
+        if(isset($_POST['submitForm'])){
+            $db=db_connect();
+            $req=$db->prepare("SELECT * FROM user WHERE mail = :email"); //":" placeholder
+            $req->execute(array("email" => $_POST['email']));
+            while($data=$req->fetch()){
+                echo $data['password'];
+            }
+        }
+    }
+    connect();
       echo "Bienvenue sur ton gestionnaire de budget !<br/>";
       echo "Entre tes identifiants :";
     ?>
@@ -25,3 +37,4 @@
 
   </body>
 </html>
+
